a4d8a8a69b610edf789c5c9e9798ea8b35cbde7b
李敏 <1512510162@qq.com>
2018-04-17 09:39:37 +0800

    Initial Commit

---
+++ CoreData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreData
+//
+//  Created by 李敏 on 2018/4/17.
+//  Copyright © 2018年 Tongle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
